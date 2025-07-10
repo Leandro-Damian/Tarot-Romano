bcbda16e6a24b5755baf329fbd183bb8ede0a8e3
Leandro Chumbita <chumbitadev@gmail.com>
2025-07-10 16:20:16 -0300

    Se soluciono el boton de scrollUp y ver como funciona el de whatsapp

--- Tarot-Romano/assets/js/main.js
+++ Tarot-Romano/assets/js/main.js
@@ -55,50 +55,25 @@ window.addEventListener('scroll', scrollUp)
 /*=============== SCROLL SELECTIONS ACTIVE LINK  ===============*/
 const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () => {
-    const scrollDown = window.scrollY;
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
 
-    sections.forEach(current => {
+    sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id'),
-              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+          sectionTop = current.offsetTop - 58,
+          sectionId = current.getAttribute('id'),
+          sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-            sectionClass.classList.add('active-link');
-        } else {
-            sectionClass.classList.remove('active-link');
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
         }
-    });
+    })    
 }
-window.addEventListener('scroll', scrollActive);
+window.addEventListener('scroll', scrollActive)
 
-
-
-
-
-
-
-
-// const scrollActive = () =>{
-//     const scrollDown = window.scrollY
-
-//     sections.forEach(current =>{
-//         const sectionHeight = current.offsetHeight,
-//           sectionTop = current.offsetTop - 58,
-//           sectionId = current.getAttribute('id'),
-//           sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-
-//         if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-//             sectionClass.classList.add('active-link')
-//         }else{
-//             sectionClass.classList.remove('active-link')
-//         }
-//     })    
-// }
-// window.addEventListener('scroll', scrollActive)
-
-/*=============== SCROLL SELECTIONS ACTIVE LINK  ===============*/
+/*=============== SCROLL REVEAL ANIMATION  ===============*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
@@ -109,8 +84,6 @@ const sr = ScrollReveal({
 
 sr.reveal(`.home__data, .popular__container, .footer`)
 sr.reveal(`.home__tarot-img`, {delay: 700, distance: '100px', origin: 'right'})
-// sr.reveal(`.home__pizza`, {delay: 1400, distance: '100px', origin: 'bottom', rotate: {z: -90}})
-// sr.reveal(`.home__ingredient`, {delay: 2000, interval: 100})
 sr.reveal(`.about__data, .recipe__list, .contact__data`, {origin: 'right'})
 sr.reveal(`.about__img, .recipe__img, .contact__image`, {origin: 'left'})
 sr.reveal(`.products__card`, {interval: 100})
